4e3530fab3aa3cee8ae99760c6d31a23672a0bce
dfink100 <dfink100@gmail.com>
2021-08-23 05:16:30 +0000

    Push from SwaggerHub.

---
+++ nodejs-server/controllers/Pet.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Pet = require('../service/PetService');
+
+module.exports.addPet = function addPet (req, res, next, body) {
+  Pet.addPet(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.deletePet = function deletePet (req, res, next, petId, api_key) {
+  Pet.deletePet(petId, api_key)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.findPetsByStatus = function findPetsByStatus (req, res, next, status) {
+  Pet.findPetsByStatus(status)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.findPetsByTags = function findPetsByTags (req, res, next, tags) {
+  Pet.findPetsByTags(tags)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getPetById = function getPetById (req, res, next, petId) {
+  Pet.getPetById(petId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.updatePet = function updatePet (req, res, next, body) {
+  Pet.updatePet(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.updatePetWithForm = function updatePetWithForm (req, res, next, petId) {
+  Pet.updatePetWithForm(petId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.uploadFile = function uploadFile (req, res, next, body, petId) {
+  Pet.uploadFile(body, petId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
