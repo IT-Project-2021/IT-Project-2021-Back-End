337d046d4095fe353ca43b6a0588b512fd01a64a
Daniel Fink <dfink100@gmail.com>
2021-09-17 14:19:57 +1000

    Remove meeting API
    For some reason was merged into main before finished.

--- index.route.js
+++ index.route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const userRoutes = require('./server/user/user.route');
 const authRoutes = require('./server/auth/auth.route');
 const personRoutes = require('./server/person/person.route');
-const meetingRoutes = require('./server/meeting/meeting.route');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -21,8 +20,5 @@ router.use('/auth', authRoutes);
 
 // mount person routes at /people
 router.use('/people', personRoutes);
-
-// mounter meeting routes at /meetings
-router.use('/meetings', meetingRoutes)
 
 module.exports = router;
